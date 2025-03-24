181becb882b1ee4c8e1bd8145baff12c8a44635f
izzahfaiqputri <izzahfaiqputri@gmail.com>
2025-03-24 10:27:10 +0700

    add button Get Started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,6 +39,7 @@ const Navbar = () => {
         >
           Get Started
         </Link>
+        {/* Add Button */}
         <Link
           to='#'
           className='hidden p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight md:block'
